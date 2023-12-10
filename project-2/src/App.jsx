95041213c5ed82b30db1956b9798d6d916721d97
Delta Course <umashree.ab53@gmail.com>
2023-12-10 18:57:11 +0530

    All files committed

---
+++ project-2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ContactHeader from './Components/ContactHeader/ContactHeader';
+import Navigation from './Components/Navigation'
+
+function App() {
+
+ return ( 
+  <div>
+    <Navigation />
+    <ContactHeader />
+  </div>
+  )
+}
+
+export default App;
